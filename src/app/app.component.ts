19c6fd8a3f6245c6303c689075c6ee6658d59ea0
Shogen, Shuhei | Lethe | ESD <shuhei.shogen@rakuten.com>
2019-04-28 23:18:42 +0900

    Push in the mid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'push-sample';
+
+  readonly VAPID_PUBLIC_KEY = 'BBxV4ww4JSlETmuqOUp1U9Y9uDwYYnVW4IvAmHxBBjeGuD7wNqdbkVX2a2IggCCTZaUPiqb38XLUWE6uBELZ2tQ';
+
+  constructor(
+    private swPush: SwPush
+  ) {
+    if (this.swPush.isEnabled) {
+      this.swPush
+      .requestSubscription({
+        serverPublicKey: this.VAPID_PUBLIC_KEY
+      })
+      .then(subscription => {
+
+      })
+      .catch(console.error);
+    }
+  }
+
+  subscribeToNotifications() {
+    console.log('subscribeToNotifications');
+  }
 }
